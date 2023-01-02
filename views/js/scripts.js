48e6f376f6b057d964a5b04b80c133fd2aea06f3
Fuyukito <danilorms.dr@gmail.com>
2023-01-02 11:22:23 +0000

    Script Java

--- views/js/scripts.js
+++ views/js/scripts.js
@@ -1,36 +1,38 @@
-const pokedex = document.getElementById('pokedex');
+document.querySelector("#search").addEventListener("click", getPokemon);
 
-const fetchPokemon = () => {
-    const promises = [];
-    for (let i = 1; i <= 150; i++) {
-        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        promises.push(fetch(url).then((res) => res.json()));
-    }
-    Promise.all(promises).then((results) => {
-        const pokemon = results.map((result) => ({
-            name: result.name,
-            image: result.sprites['front_default'],
-            type: result.types.map((type) => type.type.name).join(', '),
-            id: result.id
-        }));
-        displayPokemon(pokemon);
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function lowerCaseName(string) {
+  return string.toLowerCase();
+}
+
+function getPokemon(e) {
+  const name = document.querySelector("#pokeName").value;
+  const pokeName = lowerCaseName(name);
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector(".pokemonBox").innerHTML = `
+      <div>
+        <img
+          src="${data.sprites.other["official-artwork"].front_default}"
+          alt="Pokemon name"
+        />
+      </div>
+      <div class="pokemonInfos">
+        <h1>${capitalizeFirstLetter(data.name)}</h3>
+        <p>Weight: ${data.weight}</p>
+      </div>`;
+    })
+    .catch((err) => {
+      document.querySelector(".pokemonBox").innerHTML = `
+      <h4>Pokemon not found 😞</h4>
+      `;
+      console.log("Pokemon not found", err);
     });
-};
 
-const displayPokemon = (pokemon) => {
-    console.log(pokemon);
-    const pokemonHTMLString = pokemon
-        .map(
-            (pokeman) => `
-        <li class="card">
-            <img class="card-image" src="${pokeman.image}"/>
-            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
-            <p class="card-subtitle">Type: ${pokeman.type}</p>
-        </li>
-    `
-        )
-        .join('');
-    pokedex.innerHTML = pokemonHTMLString;
-};
-
-fetchPokemon();
+  e.preventDefault();
+}
